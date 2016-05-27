3b7d1d9afe7004791e8e7e256e0fe01a76d6e63f
zhaolei <zhaolei@localhost.localdomain>
2016-05-27 09:07:52 -0600

    first commit

---
+++ init.s
@@ -0,0 +1,49 @@
+	.file	"init.c"
+	.stabs	"kern/init.c",100,0,2,.Ltext0
+	.text
+.Ltext0:
+	.stabs	"gcc2_compiled.",60,0,0,0
+	.stabs	"int:t(0,1)=r(0,1);-2147483648;2147483647;",128,0,0,0
+	.stabs	"char:t(0,2)=r(0,2);0;127;",128,0,0,0
+	.stabs	"long int:t(0,3)=r(0,3);-2147483648;2147483647;",128,0,0,0
+	.stabs	"unsigned int:t(0,4)=r(0,4);0;4294967295;",128,0,0,0
+	.stabs	"long unsigned int:t(0,5)=r(0,5);0;4294967295;",128,0,0,0
+	.stabs	"long long int:t(0,6)=r(0,6);-0;4294967295;",128,0,0,0
+	.stabs	"long long unsigned int:t(0,7)=r(0,7);0;-1;",128,0,0,0
+	.stabs	"short int:t(0,8)=r(0,8);-32768;32767;",128,0,0,0
+	.stabs	"short unsigned int:t(0,9)=r(0,9);0;65535;",128,0,0,0
+	.stabs	"signed char:t(0,10)=r(0,10);-128;127;",128,0,0,0
+	.stabs	"unsigned char:t(0,11)=r(0,11);0;255;",128,0,0,0
+	.stabs	"float:t(0,12)=r(0,1);4;0;",128,0,0,0
+	.stabs	"double:t(0,13)=r(0,1);8;0;",128,0,0,0
+	.stabs	"long double:t(0,14)=r(0,1);12;0;",128,0,0,0
+	.stabs	"_Decimal32:t(0,15)=r(0,1);4;0;",128,0,0,0
+	.stabs	"_Decimal64:t(0,16)=r(0,1);8;0;",128,0,0,0
+	.stabs	"_Decimal128:t(0,17)=r(0,1);16;0;",128,0,0,0
+	.stabs	"void:t(0,18)=(0,18)",128,0,0,0
+	.stabs	"./inc/stdio.h",130,0,0,0
+	.stabs	"./inc/stdarg.h",130,0,0,0
+	.stabs	"va_list:t(2,1)=(2,2)=*(0,2)",128,0,0,0
+	.stabn	162,0,0,0
+	.stabn	162,0,0,0
+	.stabs	"./inc/string.h",130,0,0,0
+	.stabs	"./inc/types.h",130,0,0,0
+	.stabs	"bool:t(4,1)=(4,2)=eFalse:0,True:1,;",128,0,0,0
+	.stabs	" :T(4,3)=efalse:0,true:1,;",128,0,0,0
+	.stabs	"int8_t:t(4,4)=(0,10)",128,0,0,0
+	.stabs	"uint8_t:t(4,5)=(0,11)",128,0,0,0
+	.stabs	"int16_t:t(4,6)=(0,8)",128,0,0,0
+	.stabs	"uint16_t:t(4,7)=(0,9)",128,0,0,0
+	.stabs	"int32_t:t(4,8)=(0,1)",128,0,0,0
+	.stabs	"uint32_t:t(4,9)=(0,4)",128,0,0,0
+	.stabs	"int64_t:t(4,10)=(0,6)",128,0,0,0
+	.stabs	"uint64_t:t(4,11)=(0,7)",128,0,0,0
+	.stabs	"intptr_t:t(4,12)=(4,8)",128,0,0,0
+	.stabs	"uintptr_t:t(4,13)=(4,9)",128,0,0,0
+	.stabs	"physaddr_t:t(4,14)=(4,9)",128,0,0,0
+	.stabs	"ppn_t:t(4,15)=(4,9)",128,0,0,0
+	.stabs	"size_t:t(4,16)=(4,9)",128,0,0,0
+	.stabs	"ssize_t:t(4,17)=(4,8)",128,0,0,0
+	.stabs	"off_t:t(4,18)=(4,8)",128,0,0,0
+	.stabn	162,0,0,0
+	.stabn	162,0,0,0
